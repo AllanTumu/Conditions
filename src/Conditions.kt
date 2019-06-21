d44e2a8c176ad908a8112438fcf9568e4223f1ac
AllanTumu <allantumu1962@gmail.com>
2019-06-21 16:52:47 +0300

    Initial commit - just if

---
+++ src/Conditions.kt
@@ -0,0 +1,11 @@
+fun main (args:Array<String>){
+    //input
+    println("Input your marks")
+    var marks:Int?
+    marks = readLine()!!.toInt()
+
+    //processs
+    if (marks>95){
+        println("Excellent")
+    }
+}
