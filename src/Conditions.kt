23ff540c6e07a2d9f2803803d2b14e278478e7aa
AllanTumu <allantumu1962@gmail.com>
2019-06-21 17:05:47 +0300

    else

--- src/Conditions.kt
+++ src/Conditions.kt
@@ -4,8 +4,15 @@ fun main (args:Array<String>){
     var marks:Int?
     marks = readLine()!!.toInt()
 
-    //processs
-    if (marks>95){
+    println("Input your marks 1")
+    var marks1:Int?
+    marks1 = readLine()!!.toInt()
+
+    //process
+    if (marks>95 && marks1>80){
         println("Excellent")
+    }
+    else {
+        println("tried")
     }
 }
